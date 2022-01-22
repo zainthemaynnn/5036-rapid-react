5a85ff6ebfaa49f11401e696d9b5ae46e4a90ab0
ihabexe <ihabzeno@protonmail.com>
2022-01-21 22:41:29 -0500

    Created LedSubsystem

--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -1,28 +1,61 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 // LEDs for driver feedback, programmer like a regular Spark Motor controller and PWM output
 // Flash lights when ball is picked up, this is to be detected by detecting a CURRENT spike (momentary increase) on the intake motors
 // We will have to see what baseline current is, then see what the spike is when we get a ball. Might also have to do some 
 // simple filtering where we ignore current spike on start up (1s ignore) because that also draws large current
-// A quick 3 flash when ball is picked up.
 // Also start flashing yellow or something when 30 seconds left
 
 public class LedSubsystem implements Subsystem  {
 
-    public LedSubsystem() {
+    private static final int numberLeds = 40;
 
+    private static final AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(numberLeds);
+    private static final AddressableLED m_led = new AddressableLED(9); // change to PWM Port for LED Strip
+
+    private static boolean isFlashing = false;
+
+    public LedSubsystem() {
+        m_led.setLength(numberLeds);
+        m_led.start();
     }
 
+    public void setAllLeds(int red, int green, int blue) {
+        for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+            m_ledBuffer.setRGB(i, red, green, blue);
+        }
+    }
+
+    // A quick 3 flash when ball is picked up.
     public void ballPickUpFlash() {
-        // A quick 3 flash when ball is picked up.
-        
+        if (isFlashing) { return; }
+        int count = 3;
+        while (count > 0) {
+            setAllLeds(0, 255, 0);
+            Timer.delay(0.5);
+            count++;
+        }
+    }
+
+    public void startFlashing() {
+        while (isFlashing) {
+            setAllLeds(255, 255, 0);
+            Timer.delay(0.5);
+        }
+    }
+
+    public void endFlashing() {
+        isFlashing = false;
     }
 
     @Override
     public void periodic() {
-      // This method will be called once per scheduler run
+        m_led.setData(m_ledBuffer);
     }
 
 }
