fedad53b83ffb6a520d12e3666aef1928f795b5d
ihabexe <ihabzeno@protonmail.com>
2022-01-21 23:31:50 -0500

    Create LedCommand.java

---
+++ src/main/java/frc/robot/commands/LedCommand.java
@@ -0,0 +1,42 @@
+package frc.robot.commands;
+
+import java.util.Set;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.LedSubsystem;
+
+public class LedCommand implements Command {
+
+    private final LedSubsystem m_ledSubsystem;
+
+    public LedCommand(LedSubsystem ledSubsystem) {
+        m_ledSubsystem = ledSubsystem;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        return null;
+    }
+    
+}
